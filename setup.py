fecdcd4e81d091e7f8ddf8213e75780af90fef82
Chris McKenzie <kristopolous@yahoo.com>
2024-11-15 18:38:33 -0800

    one more rename

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="vast-sdk",
+    name="vastai-sdk",
     version="0.1.0",
     description="SDK for Vast.ai GPU Cloud Service",
     long_description=open("README.md").read(),
