55fda01377abcb0de00b3a68b66f51b35fe8cbe2
Chris McKenzie <kristopolous@yahoo.com>
2025-04-30 10:26:53 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vastai-sdk",
-    version="0.1.18",
+    version="0.1.19",
     description="SDK for Vast.ai GPU Cloud Service",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
