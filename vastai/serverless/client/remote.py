40cf0eed66cc1990ba1fe1b2e9899aa0f0861d26
Anthony Benjamin <anthony@vast.ai>
2025-11-20 13:39:50 -0800

    chore: update get mode

--- vastai/serverless/client/remote.py
+++ vastai/serverless/client/remote.py
@@ -5,6 +5,7 @@ def main():
     parser = argparse.ArgumentParser(description="Serverless Remote Dispatch")
 
     parser.add_argument("mode", choices=["deploy", "serve", "run"])
+    parser.add_argument("--remote-dispatch-file-path", default="endpoint.py", type=str)
     args = parser.parse_args()
 
     if args.mode == "deploy":
@@ -14,5 +15,4 @@ def main():
     if args.mode == "run":
         pass
 
-if __name__ == "__main__":
-    main()
+main()
