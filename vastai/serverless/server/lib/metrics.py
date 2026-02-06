bc873bff9a5ecfcd797f8cc1389470b5fa8be8e3
Lucas Armand <lucas@vast.ai>
2026-02-05 17:52:25 -0800

    new version test

--- vastai/serverless/server/lib/metrics.py
+++ vastai/serverless/server/lib/metrics.py
@@ -260,7 +260,7 @@ class Metrics:
                 "\n".join(
                     [
                         "\n" + "#" * 20 + " Worker Status " + "#" * 20,
-                        #"\n" + "I'M A OLD VERSION!" + "#" * 18,
+                        "\n" + "I'M A OLD VERSION!" + "#" * 18,
                         f"{json.dumps(log_data, indent=2)}",
                         "#" * 55,
                     ]
