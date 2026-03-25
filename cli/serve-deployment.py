5111c98bf702a4ced98a2824d15853aa39a9ec78
Edgar Lin <edgarlin2000@gmail.com>
2026-03-25 15:33:45 -0700

    flesh out serve flow a bit more

--- cli/serve-deployment.py
+++ cli/serve-deployment.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import os
+import json
 from vastai.serverless.remote.base import Config
 from vastai.serverless.remote.serve import Deployment
 
@@ -7,12 +8,14 @@ if __name__ == "__main__":
     deployment_id = os.environ.get("DEPLOYMENT_ID")
 
     # TODO: download deployment tarball
+    # Allow setting an environment variable to skip download and specify a local tarball to use for testing
 
     # TODO extract deployment tarball in current working directory, allowing absolute paths
     # We expect the deployment tarball to extract config.json into current working directory, as well as either deployment/ (if deployment is a package) or deployment.py
 
     def get_config(path: str) -> Config:
-        raise NotImplementedError("complete me")
+        with open("config.json") as f:
+            return Config(**json.load(f))
 
     config: Config = get_config("config.json")
     deployment = Deployment.lookup(config.name)
@@ -22,3 +25,6 @@ if __name__ == "__main__":
     # (1) Export ENVs that config defines, both for us and our subprocesses; also save to /etc/environment as courtesy for debuggers
     # (2) Run apt gets from config; run pip installs from config
     # (3) run scripts from config; `sh -c` if string or execvp style if list of args
+
+    worker = deployment.into_worker()
+    worker.run()
